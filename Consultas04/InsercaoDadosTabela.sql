0a134d73d54fe9b42c4a24b99285c29adacee3dc
Germanna Rebouças <reboucasgermanna@gmail.com>
2022-10-21 19:30:39 -0300

    Create InsercaoDadosTabela.sql

---
+++ Consultas04/InsercaoDadosTabela.sql
@@ -0,0 +1,20 @@
+insert into empregado values 
+('João', 999999999,'1987-06-26','rua dinosssauro', 'm', 12500.02, 999999999, null),
+('ana', 111111111,'1987-06-26','rua dinosssauro', 'm', 1200.02, 999999999, null),
+('ze', 222222222,'1987-06-26','rua dinosssauro', 'm', 17500.02, 999999999, null),
+('chico', 333333333,'1987-06-26','rua dinosssauro', 'm', 72500.02, 111111111, null),
+('francisca', 654465465,'1987-06-26','rua dinosssauro', 'm', 15500.02, 111111111, null),
+('pedrosa', 444444444,'1987-06-26','rua dinosssauro', 'm', 18000.02, 999999999, null)
+;
+
+insert into departamento values
+('tecnologia', 01, 654465465),
+('rh', 02, 444444444)
+;
+
+insert into projeto values
+('site', 11, 'internet', 01),
+('mobile', 22, 'celular', 02),
+('contratacao', 33, 'fisico', 02)
+;
+
